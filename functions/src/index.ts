b9d799efcb0659fc199806fb89b0d4f8ff04900f
下村航 <shimomurawataru@shimomurawatarunoMacBook-Pro.local>
2020-05-21 21:52:06 +0900

    お気に入り一覧表示作成中

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,77 @@
 import * as functions from 'firebase-functions';
+import admin from 'firebase-admin';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+//ログイン処理、ログインIDの有無を確認するためuseridの一覧を取得する
+export const getuserid = functions.https.onRequest((request, response) => {
+  const ref = admin.firestore().collection('/githubdb/userlist/userid');
+  ref
+    .get()
+    .then(snapshot => {
+      response.json(snapshot.docs);
+    })
+    .catch(error => {
+      response.status(500).send(error);
+    });
+  console.log();
+});
+
+//ログイン時のユーザー照会時に新規アカウントだった場合、新規アカウントを発行
+export const registration = functions.https.onRequest((request, response) => {
+  const setid = request.query.userid;
+  admin.firestore().doc('/githubdb/userlist/userid/' + setid);
+});
+
+//対象のユーザーのお気に入り一覧を取得する
+export const getsStarelist = functions.https.onRequest((request, response) => {
+  const ref = admin
+    .firestore()
+    .collection('/githubdb/userlist/userid/testacount/starlist');
+  ref
+    .get()
+    .then(snapshot => {
+      response.json(snapshot.docs);
+    })
+    .catch(error => {
+      response.status(500).send(error);
+    });
+});
+
+//対象のリポジトリに入力したコメントを追加する
+export const addComment = functions.https.onRequest((request, response) => {
+  const setRepository = request.query.repository;
+  const setComment = request.query.comment;
+  const ref = admin
+    .firestore()
+    .collection('/githubdb/userlist/userid/testacount/starlist/');
+  ref.doc('' + setRepository).update({
+    comment: setComment
+  });
+});
+
+//このアプリ内で新規お気に入りリポジトリの追加登録を行う
+export const addRepository = functions.https.onRequest((request, response) => {
+  const ref = admin
+    .firestore()
+    .collection('/articles/iymeeKlE29fg8OJKTJvJ/userid');
+  ref
+    .get()
+    .then(snapshot => {
+      response.json(snapshot.docs);
+    })
+    .catch(error => {
+      response.status(500).send(error);
+    });
+});
+
+//対象のリポジトリを消去する
+export const deleteRepository = functions.https.onRequest(
+  (request, response) => {
+    const delRepository = request.query.repository;
+    const ref = admin
+      .firestore()
+      .collection('/githubdb/userlist/userid/testacount/starlist/');
+    ref.doc('' + delRepository).delete();
+  }
+);
--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Route, Link } from 'react-router-dom';
+import axios from 'axios';
+import StarlistPayload from '../data/StarlistPayload';
 
 const Login = () => {
   return (
@@ -69,30 +71,209 @@ const Entry = () => {
 
 const stares = () => {
   var list = [];
-  //サンプルデータ
-  var data = [
-    {
-      name: "smple1",
-      resourcePath: "http*******",
-      primaryLanguage: "Java",
-      comment: "あとで確認する"
-    },
-    {
-      name: "smple2",
-      resourcePath: "http*******",
-      primaryLanguage: "React",
-      comment: "Hooks参考"
-    }
-  ];
+
+  // const data = [
+  //   {
+  //     "_fieldsProto": {
+  //       "comment": {
+  //         "stringValue": "testmemo",
+  //         "valueType": "stringValue"
+  //       },
+  //       "language": {
+  //         "stringValue": "Java",
+  //         "valueType": "stringValue"
+  //       },
+  //       "repositoryname": {
+  //         "stringValue": "testrepositoryname",
+  //         "valueType": "stringValue"
+  //       },
+  //       "url": {
+  //         "stringValue": "localhost:8080",
+  //         "valueType": "stringValue"
+  //       }
+  //     },
+  //     "_ref": {
+  //       "_firestore": {
+  //         "_settings": {
+  //           "projectId": "githubdb-d71b1",
+  //           "firebaseVersion": "8.12.1",
+  //           "libName": "gccl",
+  //           "libVersion": "3.8.0 fire/8.12.1",
+  //           "servicePath": "firestore.googleapis.com",
+  //           "port": 443,
+  //           "clientConfig": {},
+  //           "scopes": [
+  //             "https://www.googleapis.com/auth/cloud-platform",
+  //             "https://www.googleapis.com/auth/datastore"
+  //           ]
+  //         },
+  //         "_settingsFrozen": true,
+  //         "_serializer": {
+  //           "allowUndefined": false
+  //         },
+  //         "_projectId": "githubdb-d71b1",
+  //         "registeredListenersCount": 0,
+  //         "_lastSuccessfulRequest": 1589968338413,
+  //         "_backoffSettings": {
+  //           "initialDelayMs": 100,
+  //           "maxDelayMs": 60000,
+  //           "backoffFactor": 1.3
+  //         },
+  //         "_preferTransactions": false,
+  //         "_clientPool": {
+  //           "concurrentOperationLimit": 100,
+  //           "maxIdleClients": 1,
+  //           "activeClients": {},
+  //           "terminated": false,
+  //           "terminateDeferred": {
+  //             "promise": {
+  //               "domain": {
+  //                 "domain": null,
+  //                 "_events": {},
+  //                 "_eventsCount": 3,
+  //                 "members": []
+  //               }
+  //             }
+  //           }
+  //         }
+  //       },
+  //       "_path": {
+  //         "segments": [
+  //           "githubdb",
+  //           "userlist",
+  //           "userid",
+  //           "testacount",
+  //           "starlist",
+  //           "testRepositoryid"
+  //         ],
+  //         "projectId": "githubdb-d71b1",
+  //         "databaseId": "(default)"
+  //       },
+  //       "_converter": {}
+  //     },
+  //     "_serializer": {
+  //       "allowUndefined": false
+  //     },
+  //     "_readTime": {
+  //       "_seconds": 1589968338,
+  //       "_nanoseconds": 375721000
+  //     },
+  //     "_createTime": {
+  //       "_seconds": 1589707579,
+  //       "_nanoseconds": 96026000
+  //     },
+  //     "_updateTime": {
+  //       "_seconds": 1589707579,
+  //       "_nanoseconds": 96026000
+  //     }
+  //   },
+  //   {
+  //     "_fieldsProto": {
+  //       "comment": {
+  //         "stringValue": "testmemo2",
+  //         "valueType": "stringValue"
+  //       },
+  //       "language": {
+  //         "stringValue": "React",
+  //         "valueType": "stringValue"
+  //       },
+  //       "repositoryname": {
+  //         "stringValue": "testname2",
+  //         "valueType": "stringValue"
+  //       },
+  //       "url": {
+  //         "stringValue": "testurl",
+  //         "valueType": "stringValue"
+  //       }
+  //     },
+  //     "_ref": {
+  //       "_firestore": {
+  //         "_settings": {
+  //           "projectId": "githubdb-d71b1",
+  //           "firebaseVersion": "8.12.1",
+  //           "libName": "gccl",
+  //           "libVersion": "3.8.0 fire/8.12.1",
+  //           "servicePath": "firestore.googleapis.com",
+  //           "port": 443,
+  //           "clientConfig": {},
+  //           "scopes": [
+  //             "https://www.googleapis.com/auth/cloud-platform",
+  //             "https://www.googleapis.com/auth/datastore"
+  //           ]
+  //         },
+  //         "_settingsFrozen": true,
+  //         "_serializer": {
+  //           "allowUndefined": false
+  //         },
+  //         "_projectId": "githubdb-d71b1",
+  //         "registeredListenersCount": 0,
+  //         "_lastSuccessfulRequest": 1589968338413,
+  //         "_backoffSettings": {
+  //           "initialDelayMs": 100,
+  //           "maxDelayMs": 60000,
+  //           "backoffFactor": 1.3
+  //         },
+  //         "_preferTransactions": false,
+  //         "_clientPool": {
+  //           "concurrentOperationLimit": 100,
+  //           "maxIdleClients": 1,
+  //           "activeClients": {},
+  //           "terminated": false,
+  //           "terminateDeferred": {
+  //             "promise": {
+  //               "domain": {
+  //                 "domain": null,
+  //                 "_events": {},
+  //                 "_eventsCount": 3,
+  //                 "members": []
+  //               }
+  //             }
+  //           }
+  //         }
+  //       },
+  //       "_path": {
+  //         "segments": [
+  //           "githubdb",
+  //           "userlist",
+  //           "userid",
+  //           "testacount",
+  //           "starlist",
+  //           "testRepositoryid2"
+  //         ],
+  //         "projectId": "githubdb-d71b1",
+  //         "databaseId": "(default)"
+  //       },
+  //       "_converter": {}
+  //     },
+  //     "_serializer": {
+  //       "allowUndefined": false
+  //     },
+  //     "_readTime": {
+  //       "_seconds": 1589968338,
+  //       "_nanoseconds": 375721000
+  //     },
+  //     "_createTime": {
+  //       "_seconds": 1589806947,
+  //       "_nanoseconds": 434569000
+  //     },
+  //     "_updateTime": {
+  //       "_seconds": 1589806947,
+  //       "_nanoseconds": 434569000
+  //     }
+  //   }
+  // ]
+
+  const data: StarlistPayload = axios.get('https://us-central1-githubdb-d71b1.cloudfunctions.net/getsStarelist')
 
   for (var i in data) {
     list.push(
       <div>
         <p>------------------------------</p>
-        <li>リポジトリ名「{data[i].name}」</li>
-        <li>URL       「{data[i].resourcePath}」</li>
-        <li>使用言語「{data[i].primaryLanguage}」</li>
-        <li>コメント「{data[i].comment}」<button>Edit</button></li>
+        <li>リポジトリ名「{data[i]._fieldsProto.repositoryname.stringValue}」</li>
+        <li>URL       「{data[i]._fieldsProto.url.stringValue}」</li>
+        <li>使用言語「{data[i]._fieldsProto.language.stringValue}」</li>
+        <li>コメント「{data[i]._fieldsProto.comment.stringValue}」<button>Edit</button></li>
+        <li>対象ドキュメント「{data[i]._ref._path.segments[5]}」</li>
         <button>消去</button>
         <p>------------------------------</p>
       </div>
